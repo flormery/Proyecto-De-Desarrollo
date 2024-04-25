a25de1b3b8dcf587c5cbbf2bd8057f83b98193d5
flormery <meryelizabeth19@gmail.com>
2024-04-24 22:35:31 -0500

    proyecto

--- ms-coordinador/src/main/java/com/example/mscoordinador/MsCoordinadorApplication.java
+++ ms-coordinador/src/main/java/com/example/mscoordinador/MsCoordinadorApplication.java
@@ -16,9 +16,9 @@ public class MsCoordinadorApplication {
 	@Bean
 	public OpenAPI customOpenAPI() {
 		return new OpenAPI().info(new Info()
-				.title("OPEN API MICROSERVICIO CATÁLOGO")
+				.title("OPEN API MICROSERVICIO COORDINADOR")
 				.version("0.0.1")
-				.description("servicios web catálogo")
+				.description("servicios web coordinador")
 				.termsOfService("http:// swagger.io/terms")
 				.license(new License().name("Apache 2.0").url("http://springdoc.org"))
 		);
